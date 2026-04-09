22149d3f8c952724c59d39d58ed265f758c13ddf
fuqian <fuqian@users.noreply.github.com>
2026-04-10 07:22:25 +0800

    chore(build): configure output directory to dist

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -14,6 +14,7 @@ function obfuscateKey(key: string): string {
 
 export default defineConfig({
 	srcDir: 'src',
+	outDir: 'dist',
 	modules: ['@wxt-dev/module-react'],
 	webExt: {
 		chromiumProfile: chromeProfile,
