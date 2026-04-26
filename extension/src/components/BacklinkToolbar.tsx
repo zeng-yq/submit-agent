2ab0f9a33fd9fb8e3f0a8d7dbcbe8094285eff66
fuqian <fuqian@users.noreply.github.com>
2026-04-26 21:09:30 +0800

    feat: 外链分析批量分析下拉框增加 2000 选项

--- extension/src/components/BacklinkToolbar.tsx
+++ extension/src/components/BacklinkToolbar.tsx
@@ -161,6 +161,7 @@ export function BacklinkToolbar({
 							<option value={200}>200</option>
 							<option value={500}>500</option>
 							<option value={1000}>1000</option>
+							<option value={2000}>2000</option>
 						</select>
 						<Button
 							variant="default"
